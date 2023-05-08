73578129224d726a60fab1de6653d2258043d9d0
TeamRemo <131782974+TeamRemo@users.noreply.github.com>
2023-05-08 16:39:52 +0530

    Update chatbot.py

--- EmikoRobot/modules/chatbot.py
+++ EmikoRobot/modules/chatbot.py
@@ -314,7 +314,7 @@ async def inuka(client, message):
     response = response.replace("I'm married to my job.", "I'm married with Dᴀɴԍᴇʀous Roʙoт")
     response = response.replace("I'm single.", "My husband is Dᴀɴԍᴇʀous Roʙoт")
     response = response.replace("DᴀɴԍᴇʀousRoʙoт.ai", "Dᴀɴԍᴇʀous")
-    response = response.replace("I live in San Francisco, California.", "I live in ur group)
+    response = response.replace("I live in San Francisco, California.", "I live in ur group")
     response = response.replace("I made myself", "I was Created by Tᴇᴀм Rᴇмo)
     response = response.replace(
             "Hi, my friend! Do you want me to tell you a joke?",
